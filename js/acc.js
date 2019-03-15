dbab1f7227af545c927cd4c32b41fef15c820509
dchesnokov <dima3141592@gmail.com>
2019-03-16 00:48:35 +0200

    refactoring

--- js/acc.js
+++ js/acc.js
@@ -1,36 +1,79 @@
 window.onload = init;
-let incom = [],
-    expenses = [];
 
-function init(){
-    let button = document.getElementById("addButton")
-    button.onclick = handleButtonClick;
-    let localValueP = localStorage.getItem('plus');
-    let localValueM = localStorage.getItem('minus');
-  console.log(localValueP);
-  console.log(localValueM); //"myValue"
-  console.log(incom);
+const INC_STORAGE_NAME = 'incomes';
+const EXP_STORAGE_NAME = 'expenses';
+const INC_ID = 'js_incomes';
+const EXP_ID = 'js_expenses';
+
+let locStorageInc = localStorage.getItem(INC_STORAGE_NAME),
+    locStorageExp = localStorage.getItem(EXP_STORAGE_NAME),
+    incomesArr = locStorageInc ? locStorageInc.split(',') : [],
+    expensesArr = locStorageExp ? locStorageExp.split(',') : [];
+
+function init() {
+    itemsToDisplay(INC_ID, incomesArr);
+    itemsToDisplay(EXP_ID, expensesArr);
+
+    let addButton = document.getElementById("addButton");
+    addButton.onclick = handleClick;
 }
 
-function handleButtonClick() {
-  let textInput = document.getElementById("NameInput"),
-      a = textInput.value,
-      select = document.getElementById("selectInput"),
-      b = selectInput.value;
+function handleClick() {
+    let operatorElem = document.getElementById("js_operator"),
+        operator = operatorElem.value,
+        valElem = document.getElementById("js_input_val"),
+        value = valElem.value;
 
-  if (b == "plus") {
-    incom.push(a, );
-    let c = document.getElementById("incom").firstElementChild,
-        htmlCode = "<li>"+"+"+a+"</li>";
-    c.insertAdjacentHTML('afterBegin',htmlCode);
+    if (!checkValue(value)) {
+        alert('Пожалуйста используйте только цифры.');
+        valElem.value = '';
 
-    localStorage.setItem(b,incom);
-  }
-  else {
-    expenses.push(a);
-    let d = document.getElementById("expenses").firstElementChild,
-        htmlCode1 = "<li>"+"-"+a+"</li>";
-    d.insertAdjacentHTML('afterBegin',htmlCode1);
-    localStorage.setItem(b,expenses);
-  }
-};
+        return;
+    }
+
+    let elemId = '';
+    let itemArr = [];
+
+    switch (operator) {
+        case '+':
+            incomesArr.push(value);
+            elemId = INC_ID;
+            itemArr = incomesArr;
+            break;
+        case '-' :
+            expensesArr.push(value);
+            elemId = EXP_ID;
+            itemArr = expensesArr;
+            break;
+        default:
+            return;
+    }
+
+    addToLocalStorage(elemId, itemArr);
+    appendHtml(elemId, value);
+}
+
+function itemsToDisplay(elemId, itemsArr) {
+    itemsArr.forEach(function (value) {
+        appendHtml(elemId, value)
+    });
+}
+
+function appendHtml(elemId, val) {
+    let html = "<li>" + val + "</li>";
+    let element = document.getElementById(elemId);
+
+    element.insertAdjacentHTML('afterbegin', html);
+}
+
+function addToLocalStorage(elemId, item) {
+    let elemName = (elemId === INC_ID) ? INC_STORAGE_NAME : EXP_STORAGE_NAME;
+
+    localStorage.setItem(elemName, item);
+}
+
+function checkValue(val) {
+    let reg = new RegExp('^\\d+$');
+
+    return reg.test(val);
+}
